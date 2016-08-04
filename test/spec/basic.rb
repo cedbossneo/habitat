ac91d20ea4a5cc7ff0f122b1e6a35a10f72dcdf3
Dave Parfitt <dparfitt@chef.io>
2016-08-04 14:05:23 -0400

    wtf whitespace Signed-off-by: Dave Parfitt <dparfitt@chef.io>

--- test/spec/basic.rb
+++ test/spec/basic.rb
@@ -18,21 +18,21 @@ end
 describe "Habitat CLI" do
     # TODO: maybe extract this into a module in the future
     before(:all) do
-		platform.common_setup()
+        platform.common_setup()
         #to see all command output:
         #platform.cmd_debug = true
     end
 
     after(:all) do
-		platform.common_teardown()
+        platform.common_teardown()
     end
 
-	after(:each) do |example|
-		if example.exception
-			puts "Detected failed examples, keeping environment"
-			platform.cleanup = false
-		end
-	end
+    after(:each) do |example|
+        if example.exception
+            puts "Detected failed examples, keeping environment"
+            platform.cleanup = false
+        end
+    end
 
     # these are in RSpec instead of Inspec because we
     # keep some platform independent paths inside the platform.
@@ -50,9 +50,9 @@ describe "Habitat CLI" do
     end
 
     context "package functionality" do
-		# this is example is somewhat larger than desired, however, it exercises
-		# build/install/start with a simple package, instead of downloading a
-		# prebuilt one from the Depot
+        # this is example is somewhat larger than desired, however, it exercises
+        # build/install/start with a simple package, instead of downloading a
+        # prebuilt one from the Depot
         it "should build, install and start a simple service without failure" do
 
             # TODO: this test WILL FAIL if a supervisor is already running.
@@ -62,38 +62,38 @@ describe "Habitat CLI" do
             # building a package can take quite awhile, let's bump the timeout to
             # 60 seconds to be sure we finish in time.
             result = platform.cmd_expect("studio build fixtures/simple_service",
-                                   "I love it when a plan.sh comes together",
-                                   :timeout_seconds => 60)
+                                         "I love it when a plan.sh comes together",
+                                         :timeout_seconds => 60)
             expect(result.exited?).to be true
             expect(result.exitstatus).to eq 0
 
-			last_build = HabTesting::Utils::parse_last_build()
-			puts last_build if platform.cmd_debug
-			expect(last_build["pkg_origin"]).to eq platform.hab_origin
-			expect(last_build["pkg_name"]).to eq "simple_service"
-			expect(last_build["pkg_version"]).to eq "0.0.1"
+            last_build = HabTesting::Utils::parse_last_build()
+            puts last_build if platform.cmd_debug
+            expect(last_build["pkg_origin"]).to eq platform.hab_origin
+            expect(last_build["pkg_name"]).to eq "simple_service"
+            expect(last_build["pkg_version"]).to eq "0.0.1"
 
-			built_artifact = Pathname.new("results").join(last_build["pkg_artifact"])
-			expect(File.exist?(built_artifact)).to be true
-			result = platform.cmd_expect("pkg install ./results/#{last_build["pkg_artifact"]}",
-                "Install of #{platform.hab_origin}/simple_service/0.0.1/#{last_build["pkg_release"]} complete with 1 packages installed",
-                :kill_when_found => false)
+            built_artifact = Pathname.new("results").join(last_build["pkg_artifact"])
+            expect(File.exist?(built_artifact)).to be true
+            result = platform.cmd_expect("pkg install ./results/#{last_build["pkg_artifact"]}",
+                                         "Install of #{platform.hab_origin}/simple_service/0.0.1/#{last_build["pkg_release"]} complete with 1 packages installed",
+                                         :kill_when_found => false)
             expect(result.exited?).to be true
             expect(result.exitstatus).to eq 0
 
-    		installed_path = Pathname.new(platform.hab_pkg_path).join(
-								platform.hab_origin,
-								last_build["pkg_name"],
-								last_build["pkg_version"],
-								last_build["pkg_release"])
-			expect(File.exist?(installed_path)).to be true
+            installed_path = Pathname.new(platform.hab_pkg_path).join(
+                platform.hab_origin,
+                last_build["pkg_name"],
+                last_build["pkg_version"],
+                last_build["pkg_release"])
+            expect(File.exist?(installed_path)).to be true
 
             # this should start relatively quickly, so we'll use the default timeout
             # This is a long running process, so kill it when we've found the output
             # that we're looking for.
-			result = platform.cmd_expect("start #{platform.hab_origin}/simple_service",
-                                  "Shipping out to Boston",
-                                  :kill_when_found => true)
+            result = platform.cmd_expect("start #{platform.hab_origin}/simple_service",
+                                         "Shipping out to Boston",
+                                         :kill_when_found => true)
             # don't check the process status here, we killed it!
         end
     end
--- test/spec/platform.rb
+++ test/spec/platform.rb
@@ -7,185 +7,186 @@ require 'time'
 
 module HabTesting
 
-	module Utils
+    module Utils
 
         # parse a ./results/last_build.env file, split lines on `=`
         # and return a hash containing all key/values
-		def self.parse_last_build
-			results = {}
-			## TODO: we should have a test root dir var
-			File.open("results/last_build.env", "r") do |f|
-				f.each_line do |line|
-					chunks = line.split("=")
-					results[chunks[0].strip()] = chunks[1].strip()
-				end
-			end
-			return results
-		end
-	end
+        def self.parse_last_build
+            results = {}
+            ## TODO: we should have a test root dir var
+            File.open("results/last_build.env", "r") do |f|
+                f.each_line do |line|
+                    chunks = line.split("=")
+                    results[chunks[0].strip()] = chunks[1].strip()
+                end
+            end
+            return results
+        end
+    end
 
 
     # The intent of the Platform class is to store any platform-independent
     # variables.
-	class Platform
+    class Platform
         include Singleton
         # path to the `hab` command
-		attr_accessor :hab_bin;
+        attr_accessor :hab_bin;
         # TODO: unusued
-		attr_accessor :hab_key_cache;
+        attr_accessor :hab_key_cache;
         # A unique testing organization
-		attr_accessor :hab_org;
+        attr_accessor :hab_org;
         # A unique testing origin
-		attr_accessor :hab_origin;
+        attr_accessor :hab_origin;
         # The path to installed packages, (ex: /hab/pkgs on Linux)
-		attr_accessor :hab_pkg_path;
+        attr_accessor :hab_pkg_path;
         # TODO: path to the build command
-		attr_accessor :hab_plan_build;
+        attr_accessor :hab_plan_build;
         # A unique testing ring name
-		attr_accessor :hab_ring;
+        attr_accessor :hab_ring;
         # TODO: The root location of Habitat data, HAB_ROOT_PATH
-		attr_accessor :hab_root_path;
+        attr_accessor :hab_root_path;
         # A unique testing service group
-		attr_accessor :hab_service_group;
+        attr_accessor :hab_service_group;
         # TODO: the location of the studio root
-		attr_accessor :hab_studio_root;
+        attr_accessor :hab_studio_root;
         # path to the `hab-sup` command
-		attr_accessor :hab_sup_bin;
+        attr_accessor :hab_sup_bin;
         # A unique testing user
-		attr_accessor :hab_user;
+        attr_accessor :hab_user;
 
         # command output logs are stored in this directory
-		attr_accessor :log_dir;
+        attr_accessor :log_dir;
         # The filename currently be used to log command output.
         # This file is stored in @log_dir
-		attr_accessor :log_name;
+        attr_accessor :log_name;
 
         # if true, display command output
         attr_accessor :cmd_debug;
         # default timeout for child processes before failing
         attr_accessor :cmd_timeout_seconds;
 
-		# if there is an example failure, don't cleanup the state on
-		# disk if @cleanup is set to false
-		attr_accessor :cleanup;
+        # if there is an example failure, don't cleanup the state on
+        # disk if @cleanup is set to false
+        attr_accessor :cleanup;
 
         # generate a unique name for use in testing
-		def unique_name()
-			SecureRandom.uuid
-		end
+        def unique_name()
+            SecureRandom.uuid
+        end
 
         # return a list of HAB_ environment vars
-		def env_vars()
-			#TODO: share these between Inspec and Rspec?
-			return %w(HAB_AUTH_TOKEN
-					HAB_CACHE_KEY_PATH
-					HAB_DEPOT_URL
-					HAB_ORG
-					HAB_ORIGIN
-					HAB_ORIGIN_KEYS
-					HAB_RING
-					HAB_RING_KEY
-					HAB_ROOT_PATH
-					HAB_STUDIOS_HOME
-					HAB_STUDIO_ROOT
-					HAB_USER)
-		end
+        def env_vars()
+            #TODO: share these between Inspec and Rspec?
+            return %w(HAB_AUTH_TOKEN
+                    HAB_CACHE_KEY_PATH
+                    HAB_DEPOT_URL
+                    HAB_ORG
+                    HAB_ORIGIN
+                    HAB_ORIGIN_KEYS
+                    HAB_RING
+                    HAB_RING_KEY
+                    HAB_ROOT_PATH
+                    HAB_STUDIOS_HOME
+                    HAB_STUDIO_ROOT
+                    HAB_USER)
+        end
 
         # display testing parameters upon startup
-		def banner()
-			puts "-" * 80
-			puts "Test params:"
-			self.instance_variables.sort.each do |k|
-				puts "#{k[1..-1]} = #{self.instance_variable_get(k)}"
-			end
-			puts "Logging command output to #{self.log_file_name()}"
-			puts "-" * 80
-		end
-	end
+        def banner()
+            puts "-" * 80
+            puts "Test params:"
+            self.instance_variables.sort.each do |k|
+                puts "#{k[1..-1]} = #{self.instance_variable_get(k)}"
+            end
+            puts "Logging command output to #{self.log_file_name()}"
+            puts "-" * 80
+        end
 
-	class LinuxPlatform < Platform
-		def initialize
-			@hab_root_path = Dir.mktmpdir("hab_test_root")
-
-			@hab_bin="/src/components/hab/target/debug/hab"
-			#@hab_key_cache = "#{@hab_root_path}/hab/cache/keys"
-			@hab_org = "org_#{unique_name()}"
-			@hab_origin = "origin_#{unique_name()}"
-			#@hab_plan_build = "/src/components/plan-build/bin/hab-plan-build.sh"
-			@hab_pkg_path = "/hab/pkgs"
-			@hab_ring = "ring_#{unique_name()}"
-			# todo
-			@hab_service_group = "service_group_#{unique_name()}"
-			#@hab_studio_root = Dir.mktmpdir("hab_test_studio")
-			@hab_sup_bin = "/src/components/sup/target/debug/hab-sup"
-			@hab_user = "user_#{unique_name()}"
-
-			@log_name = "hab_test-#{Time.now.utc.iso8601.gsub(/\:/, '-')}.log"
-			@log_dir = "./logs"
-
-			@cleanup = true
-            @cmd_debug = false
-            @cmd_timeout_seconds = 30
-			banner()
-		end
 
         # Common setup for tests, including setting a test origin
         # and key generation.
         # # TODO: move to base class
-		def common_setup
-			ENV['HAB_ORIGIN'] = @hab_origin
-			cmd_expect("origin key generate #{@hab_origin}",
-				"Generated origin key pair #{@hab_origin}")
-			cmd_expect("user key generate #{@hab_user}",
-				"Generated user key pair #{@hab_user}")
-			cmd_expect("ring key generate #{@hab_ring}",
-				"Generated ring key pair #{@hab_ring}")
-			# remove the studio if it already exists
-			cmd("studio rm #{@hab_origin}")
-			#puts "Creating new studio, this may take a few minutes"
-			#ctx.cmd("studio -k #{ctx.hab_origin} new")
-			#puts "Setup complete"
-			puts "-" * 80
-		end
+        def common_setup
+            ENV['HAB_ORIGIN'] = @hab_origin
+            cmd_expect("origin key generate #{@hab_origin}",
+                       "Generated origin key pair #{@hab_origin}")
+            cmd_expect("user key generate #{@hab_user}",
+                       "Generated user key pair #{@hab_user}")
+            cmd_expect("ring key generate #{@hab_ring}",
+                       "Generated ring key pair #{@hab_ring}")
+            # remove the studio if it already exists
+            cmd("studio rm #{@hab_origin}")
+            #puts "Creating new studio, this may take a few minutes"
+            #ctx.cmd("studio -k #{ctx.hab_origin} new")
+            #puts "Setup complete"
+            puts "-" * 80
+        end
 
         # Common teardown for tests
-		def common_teardown
-			if @cleanup
-				puts "Clearing test environment"
-				ENV.delete('HAB_ORIGIN')
-				# TODO
-				#`rm -rf ./results`
-				# TODO: kill the studio only if all tests pass?
-				#ctx.cmd("studio rm")
-			else
-				puts "WARNING: not cleaning up testing environment"
-			end
-		end
+        def common_teardown
+            if @cleanup
+                puts "Clearing test environment"
+                ENV.delete('HAB_ORIGIN')
+                # TODO
+                #`rm -rf ./results`
+                # TODO: kill the studio only if all tests pass?
+                #ctx.cmd("studio rm")
+            else
+                puts "WARNING: not cleaning up testing environment"
+            end
+        end
+    end
+
+    class LinuxPlatform < Platform
+        def initialize
+            @hab_root_path = Dir.mktmpdir("hab_test_root")
+
+            @hab_bin="/src/components/hab/target/debug/hab"
+            #@hab_key_cache = "#{@hab_root_path}/hab/cache/keys"
+            @hab_org = "org_#{unique_name()}"
+            @hab_origin = "origin_#{unique_name()}"
+            #@hab_plan_build = "/src/components/plan-build/bin/hab-plan-build.sh"
+            @hab_pkg_path = "/hab/pkgs"
+            @hab_ring = "ring_#{unique_name()}"
+            # todo
+            @hab_service_group = "service_group_#{unique_name()}"
+            #@hab_studio_root = Dir.mktmpdir("hab_test_studio")
+            @hab_sup_bin = "/src/components/sup/target/debug/hab-sup"
+            @hab_user = "user_#{unique_name()}"
+
+            @log_name = "hab_test-#{Time.now.utc.iso8601.gsub(/\:/, '-')}.log"
+            @log_dir = "./logs"
+
+            @cleanup = true
+            @cmd_debug = false
+            @cmd_timeout_seconds = 30
+            banner()
+        end
 
 
         # execute a `hab` subcommand and wait for the process to finish
-		def cmd(cmdline, **cmd_options)
+        def cmd(cmdline, **cmd_options)
             debug = cmd_options[:debug] || @cmd_debug
 
-			if debug then
-				puts "X" * 80
-				puts `env`
-				puts "X" * 80
-			end
+            if debug then
+                puts "X" * 80
+                puts `env`
+                puts "X" * 80
+            end
 
-			fullcmdline = "#{@hab_bin} #{cmdline} | tee -a #{log_file_name()} 2>&1"
-			# record the command we'll be running in the log file
-			`echo #{fullcmdline} >> #{log_file_name()}`
-			puts "Running: #{fullcmdline}"
-			pid = spawn(fullcmdline)
-			Process.wait pid
-			return $?
-		end
+            fullcmdline = "#{@hab_bin} #{cmdline} | tee -a #{log_file_name()} 2>&1"
+            # record the command we'll be running in the log file
+            `echo #{fullcmdline} >> #{log_file_name()}`
+            puts "Running: #{fullcmdline}"
+            pid = spawn(fullcmdline)
+            Process.wait pid
+            return $?
+        end
 
         # execute a possibly long-running process and wait for a particular string
         # in it's output. If the output is found, kill the process and return
         # it's exit status. Otherwise, raise an exception so specs fail quickly.
-		def cmd_expect(cmdline, desired_output, **cmd_options)
+        def cmd_expect(cmdline, desired_output, **cmd_options)
             puts "X" * 80 if @cmd_debug
             puts cmd_options if @cmd_debug
 
@@ -198,83 +199,83 @@ module HabTesting
             #puts "TIMEOUT = #{timeout}"
             #puts "DBEUG = #{debug}"
 
-			if debug then
-				puts "X" * 80
-				puts `env`
-				puts "X" * 80
-			end
+            if debug then
+                puts "X" * 80
+                puts `env`
+                puts "X" * 80
+            end
 
-			fullcmdline = "#{@hab_bin} #{cmdline} | tee -a #{log_file_name()} 2>&1"
-			# record the command we'll be running in the log file
-			`echo #{fullcmdline} >> #{log_file_name()}`
-			puts "Running: #{fullcmdline}"
+            fullcmdline = "#{@hab_bin} #{cmdline} | tee -a #{log_file_name()} 2>&1"
+            # record the command we'll be running in the log file
+            `echo #{fullcmdline} >> #{log_file_name()}`
+            puts "Running: #{fullcmdline}"
 
-			begin
-				Open3.popen3(fullcmdline) do |stdin, stdout, stderr, wait_thread|
-					puts "Started child process" if debug
-					found = false
-					begin
-						Timeout::timeout(timeout) do
-							loop do
-								line = stdout.readline()
-								puts line if debug
-								if line.include?(desired_output) then
+            begin
+                Open3.popen3(fullcmdline) do |stdin, stdout, stderr, wait_thread|
+                    puts "Started child process" if debug
+                    found = false
+                    begin
+                        Timeout::timeout(timeout) do
+                            loop do
+                                line = stdout.readline()
+                                puts line if debug
+                                if line.include?(desired_output) then
                                     if kill_when_found then
                                         puts "Sending a TERM to child process" if debug
                                         Process.kill('TERM', wait_thread.pid)
                                         found = true
-									    break
+                                        break
                                     else
                                         # let the process finish, or timeout
                                         found = true
                                     end
-								end
-							end
-						end
-					rescue EOFError
+                                end
+                            end
+                        end
+                    rescue EOFError
                         if found then
                             return wait_thread.value
                         else
-						    raise "Process finished without finding desired output: #{desired_output}"
+                            raise "Process finished without finding desired output: #{desired_output}"
                         end
-					rescue Timeout::Error
+                    rescue Timeout::Error
                         # TODO: do timeouts always return failure?
-						puts "Timeout" if debug
-						Process.kill('TERM', wait_thread.pid)
-						puts "Child process killed" if debug
-		                raise "Proces timeout waiting for desired output: #{desired_output}"
-					end
+                        puts "Timeout" if debug
+                        Process.kill('TERM', wait_thread.pid)
+                        puts "Child process killed" if debug
+                        raise "Proces timeout waiting for desired output: #{desired_output}"
+                    end
 
-					if found == true then
-						puts "\tFound: #{desired_output}" if debug
-						return wait_thread.value
-					else
-		                raise "Output not found: #{desired_output}"
-					end
-				end
-			end
-		end
+                    if found == true then
+                        puts "\tFound: #{desired_output}" if debug
+                        return wait_thread.value
+                    else
+                        raise "Output not found: #{desired_output}"
+                    end
+                end
+            end
+        end
 
         # generate a unique log file name in the given log_dir
-		def log_file_name()
-			File.join(@log_dir, @log_name)
-		end
+        def log_file_name()
+            File.join(@log_dir, @log_name)
+        end
 
-		def mk_temp_dir()
-			# TODO: remove temp directory before creating
-			# TODO: keep track of temp files and remove them upon success?
-			dir = Dir.mktmpdir("hab_test")
-			puts "Temp dir = #{dir}"
-			return dir
-		end
+        def mk_temp_dir()
+            # TODO: remove temp directory before creating
+            # TODO: keep track of temp files and remove them upon success?
+            dir = Dir.mktmpdir("hab_test")
+            puts "Temp dir = #{dir}"
+            return dir
+        end
 
-	end
+    end
 
-	class WindowsPlatform
-		def initialize
+    class WindowsPlatform
+        def initialize
             # :-(
-			raise "Windows platform not implemented"
-		end
-	end
+            raise "Windows platform not implemented"
+        end
+    end
 
 end # module HabTesting
